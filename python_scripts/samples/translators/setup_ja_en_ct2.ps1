a277264a65e9e08a1289d059dbc2fc5c740373ee
Jethro Estrada <jethroestrada237@gmail.com>
2025-12-08 10:08:13 +0800

    Updates

--- python_scripts/samples/translators/setup_ja_en_ct2.ps1
+++ python_scripts/samples/translators/setup_ja_en_ct2.ps1
@@ -1,8 +1,11 @@
 # Download the model zip
 Invoke-WebRequest -Uri "https://object.pouta.csc.fi/OPUS-MT-models/ja-en/opus-2019-12-18.zip" -OutFile "opus-2019-12-18.zip"
 
-# Extract (built-in since Windows 10 / Server 2016)
+# Extract
 Expand-Archive -Path "opus-2019-12-18.zip" -DestinationPath "." -Force
 
 # Convert to CTranslate2 format
 ct2-opus-mt-converter --model_dir . --output_dir ja_en_ct2
+
+# ←←← This line deletes the zip when you're done
+Remove-Item -Path "opus-2019-12-18.zip" -Force
