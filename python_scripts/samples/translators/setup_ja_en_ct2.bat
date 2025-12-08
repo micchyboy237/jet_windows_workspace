249976fb98e37027365103f66c6afad7e8d67873
Jethro Estrada <jethroestrada237@gmail.com>
2025-12-08 10:05:42 +0800

    Updates

--- python_scripts/samples/translators/setup_ja_en_ct2.bat
+++ python_scripts/samples/translators/setup_ja_en_ct2.bat
@@ -1,4 +1,8 @@
-wget https://object.pouta.csc.fi/OPUS-MT-models/ja-en/opus-2019-12-18.zip
-unzip opus-2019-12-18.zip
+@echo off
+curl -L -o opus-2019-12-18.zip https://object.pouta.csc.fi/OPUS-MT-models/ja-en/opus-2019-12-18.zip
 
+:: Extract the zip (tar works with zip files on recent Windows)
+tar -xf opus-2019-12-18.zip
+
+:: Convert
 ct2-opus-mt-converter --model_dir . --output_dir ja_en_ct2
