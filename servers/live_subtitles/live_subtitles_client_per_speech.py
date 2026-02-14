949a040bfc4b4d57110cc5b53db857736844d6d9
Jethro Estrada <jethroestrada237@gmail.com>
2026-02-14 15:44:08 +0800

    Updates

--- servers/live_subtitles/live_subtitles_client_per_speech.py
+++ servers/live_subtitles/live_subtitles_client_per_speech.py
@@ -1271,8 +1271,7 @@ async def send_audio_chunk(
         "is_final": is_final,
         "context_prompt": context_prompt,
         "segment_num": segment_num,
-        # "utterance_id": current_utterance_id,
-        "utterance_id": str(uuid.uuid4()),
+        "utterance_id": current_utterance_id,
         "normalized_rms": normalized_rms,
         "avg_vad_confidence": avg_vad,
     }
