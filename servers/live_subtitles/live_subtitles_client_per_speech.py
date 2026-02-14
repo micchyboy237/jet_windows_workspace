ca53a559b1c1c1631e42dca64bde4128c9d70605
Jethro Estrada <jethroestrada237@gmail.com>
2026-02-14 16:00:50 +0800

    Updates

--- servers/live_subtitles/live_subtitles_client_per_speech.py
+++ servers/live_subtitles/live_subtitles_client_per_speech.py
@@ -55,7 +55,7 @@ CONTINUOUS_AUDIO_MAX_SECONDS = 320.0
 
 MAX_SPEECH_DURATION_SEC = 90.0
 CHUNK_DURATION_SEC = 6.0
-CHUNK_OVERLAP_SEC = 2.0
+CHUNK_OVERLAP_SEC = 0.0
 CONTEXT_PROMPT_MAX_WORDS = 40  # max tokens for context prompt to send to server
 
 audio_total_samples: int = 0
