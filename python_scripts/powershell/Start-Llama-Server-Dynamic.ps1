d9577e6c247446c87d7a10fcb52b819bf4714f4a
Jethro Estrada <jethroestrada237@gmail.com>
2026-07-18 20:21:14 +0800

    Updates

--- python_scripts/powershell/Start-Llama-Server-Dynamic.ps1
+++ python_scripts/powershell/Start-Llama-Server-Dynamic.ps1
@@ -1,1 +1,6 @@
-llama-server --models-preset models.ini --models-max 1 --host 0.0.0.0 --port 8080
+param(
+    [Alias('p')]
+    [int]$Port = 8080
+)
+
+& llama-server --models-preset models.ini --models-max 1 --host 0.0.0.0 --port $Port
