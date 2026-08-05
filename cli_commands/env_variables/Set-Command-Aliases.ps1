e9b8437ac243d3971c9a1b5c8ee54c1cf5d9dc5c
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-05 15:46:17 +0800

    Updates

--- cli_commands/env_variables/Set-Command-Aliases.ps1
+++ cli_commands/env_variables/Set-Command-Aliases.ps1
@@ -1,10 +1,26 @@
 # Set-Command-Aliases.ps1
 
-Set-Alias which Get-Command
+# Display full executable path (like Unix which)
+function which {
+    param(
+        [Parameter(Mandatory)]
+        [string]$Name,
+        [switch]$All
+    )
 
-# Get full executable path
-# which psql
+    $params = @{ Name = $Name; ErrorAction = 'SilentlyContinue' }
+    if ($All) { $params['All'] = $true }
 
+    $commands = Get-Command @params
+    if (-not $commands) {
+        Write-Warning "which: no $Name found"
+        return
+    }
+
+    $commands.Source
+}
+
+# Create files and directories (like Unix touch)
 Set-Alias -Name touch -Value New-Item
 
 # Create single file
@@ -14,4 +30,4 @@ Set-Alias -Name touch -Value New-Item
 # touch file1.txt, file2.txt, file3.txt
 
 # Autocreate subdirectories
-# touch -Force -Path ".\deep\nested\folder\myfile.txt" 
+# touch -Force -Path ".\deep\nested\folder\myfile.txt"
