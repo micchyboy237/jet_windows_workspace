b3e871390b9d28153723b784868d6bf59cf6a7dd
Jethro Estrada <jethroestrada237@gmail.com>
2026-08-06 12:23:33 +0800

    Updates

--- python_scripts/powershell/Start-Llama-Server-Embedder.ps1
+++ python_scripts/powershell/Start-Llama-Server-Embedder.ps1
@@ -1,21 +1,5 @@
 # llama-server --models-preset C:\Users\druiv\Desktop\Jet_Files\Jet_Windows_Workspace\models.embedders.ini --models-max 1 --host 0.0.0.0 --port 8081
 
-# llama-server `
-#   -m C:\Users\druiv\.cache\llama.cpp\embed_models\nomic-embed-text-v1.5.Q4_K_M.gguf `
-#   --embedding `
-#   --pooling mean `
-#   -c 8192 `
-#   -b 8192 `
-#   -ub 2048 `
-#   -np 8 `
-#   -ngl 99 `
-#   --rope-scaling yarn `
-#   --rope-freq-scale 0.75 `
-#   --host 0.0.0.0 `
-#   --port 8081 `
-#   --alias nomic-embed:1.5 `
-#   --tags embed
-
 llama-server `
   -m "C:\Users\druiv\.cache\llama.cpp\embed_models\nomic-embed-text-v1.5.Q4_K_M.gguf" `
   --embedding `
